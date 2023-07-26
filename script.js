1129ce8d38580737fbcaa4272ad6bf931ae03527
Ozuzu Destiny <oluebubedestiny.o@gmail.com>
2023-07-26 04:42:32 +0100

    rough update on about me

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 const toggle = document.querySelector(".toggle_btn");
+const skill_btn = document.querySelector(".skill_link");
+const tab_links = document.querySelector(".tab_links");
+const tab_content = document.querySelector(".tab_content");
+const skill = document.querySelector(".skill_co");
+const experience = document.querySelector(".exp_co");
+const education = document.querySelector(".edu_co");
+const ski = document.getElementById("skills");
+const exp = document.getElementById("experience");
+const edu = document.getElementById("education");
 
 toggle.addEventListener("click", function () {
   const navbar = document.querySelector(".nav_list_container");
@@ -6,3 +15,28 @@ toggle.addEventListener("click", function () {
   navbar.classList.toggle("show");
   logo.classList.toggle("hide");
 });
+
+skill.addEventListener("click", () => {
+  tab_links.classList.remove("skill_link");
+  tab_content.classList.remove("skill_con");
+  event.currentTarget.classList.add("skill_link");
+  ski.classList.add("skill_con");
+});
+
+experience.addEventListener("click", () => {
+  tab_links.classList.remove("skill_link");
+  tab_content.classList.remove("skill_con");
+  event.currentTarget.classList.add("skill_link");
+  exp.classList.add("skill_con");
+});
+education.addEventListener("click", () => {
+  tab_links.classList.remove("skill_link");
+  tab_content.classList.remove("skill_con");
+  event.currentTarget.classList.add("skill_link");
+  edu.classList.add("skill_con");
+});
+
+// function openTab(tabName) {
+//   event.currentTarget.classList.add("skill_link");
+//   document.getElementById(tabName).classList.add("skill_con");
+// }

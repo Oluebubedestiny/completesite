e60afad496ba90176386dfe0a30a33f3f63d7e0f
Ozuzu Destiny <oluebubedestiny.o@gmail.com>
2023-08-19 02:09:31 +0100

    update on my portfolio contact

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const toggle = document.querySelector(".toggle_btn");
 const nav = document.querySelector("nav");
 const navbar = document.querySelector(".nav_list_container");
+const contactBtn = document.getElementById("contact_btn");
 
 toggle.addEventListener("click", () => {
   const logo = document.querySelector(".logo_container");
@@ -19,3 +20,26 @@ window.addEventListener("scroll", () => {
 // navbar.querySelectorAll("a li").forEach((navLink) => {
 //   navLink.addEventListener("click", () => {});
 // });
+
+contactBtn.addEventListener("click", () => {
+  var params = {
+    name: document.getElementById("name_input").value,
+    email: document.getElementById("email_input").value,
+    message: document.getElementById("msg_input").value,
+  };
+
+  const serviceID = "service_d2az4he";
+  const template1D = "template_iwejtd5";
+
+  emailjs
+    .send(serviceID, template1D, params)
+    .then((res) => {
+      document.getElementById("name_input").value = "";
+      document.getElementById("email_input").value = "";
+      document.getElementById("msg_input").value = "";
+      console.log(res);
+      alert("your message sent successfully");
+    })
+    .catch((err) => console.log(err));
+});
+// function sendMail() {}
